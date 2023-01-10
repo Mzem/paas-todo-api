83b80f3cd8efce0f2003a191a3e34bdea14d3038
vinorth.varatharasan <vinorth.varatharasan@accenture.com>
2023-01-10 14:34:48 +0100

    Added environment name

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express()
 app.use(express.json())
 
 app.get("/", function (req, res) {
-  res.send(`Hello World!`)
+  res.send(`Hello World! Environnement de ` + process.env.NOM_ENV)
 })
 
 const port = process.env.PORT
